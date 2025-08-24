4901eefe9017f8ae86d97e768773b06d15ea69df
Joe S <Josephsaeid@gmail.com>
2025-08-23 18:34:38 -0700

    Add HTTPS support and fix Facebook login configuration - Google OAuth working

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Next.js 14 has app directory enabled by default
   eslint: {
-    // Don't block production builds on ESLint errors
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Don't block production builds on TS type errors
     ignoreBuildErrors: true,
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
